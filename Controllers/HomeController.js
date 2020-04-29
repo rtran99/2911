c1a6f32546b42590634e9f41c17d98497bc42fb4
offline <felix.ruttan@gmail.com>
2020-04-29 13:27:28 -0700

    pave

--- Controllers/HomeController.js
+++
@@ -1,6 +0,0 @@
-const RequestService = require('../Services/RequestService');
-
-exports.Index = async function(req, res) {
-    let reqInfo = RequestService.reqHelper(req);
-    return res.render('Home/Index', { reqInfo:reqInfo });
-};
