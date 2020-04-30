81617fd517aac336a69e34af10aa2698ba9cb0e1
offline <felix.ruttan@gmail.com>
2020-04-29 18:51:17 -0700

    fix

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
   }
 
   // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
+
 
   // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
